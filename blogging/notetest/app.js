41342d032dbd350ec1dbc9853da972aaf5776c12
DimaDennys <dumitrudenisiuc@gmail.com>
2020-06-09 18:56:54 +0100

    Part 3 Step 5 – Hook Mongo up to Node

--- blogging/notetest/app.js
+++ blogging/notetest/app.js
@@ -4,6 +4,16 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// New Code
+var monk = require('monk');
+var db = monk('localhost:27017/nodetest');
+
+// Make our db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
